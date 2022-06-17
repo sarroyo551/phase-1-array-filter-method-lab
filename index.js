1674a35215d31986a6be820fc826c810b9a28f54
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-17 12:18:29 -0400

    commit tree 70be95bb822d9b7d9a5c340187ea371e967c740e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+/*function findMatching (arrOfDri, name){
+    const result = arrOfDri.filter(driver => driver === name)
+    return result 
+}*/
+
+function findMatching (arrOfDri, name){
+    return arrOfDri.filter( function (driver) {
+    return driver.toLowerCase() === name.toLowerCase()
+    }) 
+}
+
+function fuzzyMatch (arrOfDri, name){
+    //console.log(arrOfDri, name)
+    return arrOfDri.filter(function (driver){
+        console.log(driver.substring(0, 2), name.substring(0, 4))
+    return driver.substring(0, 2) === name.substring(0, 4)
+
+    })
+}
+
+function matchName (arrOfDri, name){
+   return arrOfDri.filter(function (driver){
+      return driver.name === name
+   })
+
+}
